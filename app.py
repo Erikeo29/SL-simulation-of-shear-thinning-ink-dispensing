5fc5c56aac5603286d15216f583708e29c4e105d
Erikeo29 <erikeo29@gmail.com>
2026-03-28 08:08:02 +0100

    Fix scroll-to-top: triple declenchement (0ms, 100ms, 300ms)

--- app.py
+++ app.py
@@ -1013,12 +1013,13 @@ if st.session_state.get("_last_page") != _page_id:
     components.html(
         (
             '<script>'
-            'setTimeout(function(){'
+            'function scrollTop(){'
             'var e=window.parent.document;'
             'var targets=["section.main","[data-testid=stAppViewContainer]",".main"];'
             'targets.forEach(function(s){var el=e.querySelector(s);if(el)el.scrollTo(0,0);});'
             'e.scrollingElement.scrollTo(0,0);'
-            '},1);'
+            '}'
+            'scrollTop();setTimeout(scrollTop,100);setTimeout(scrollTop,300);'
             '</script>'
         ),
         height=0,
